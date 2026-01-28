2bf228115f31eb2505c57bb9fa867ca136b1208c
vfedotova418-png <vfedotova418@gmail.com>
2026-01-28 17:14:56 +0300

    Lists in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -1,6 +1,5 @@
 val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
 val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
-val solarSystem = rockPlanets + gasPlanets
 val newSolarSystem = arrayOf(
     "Mercury",
     "Venus",
@@ -14,15 +13,32 @@ val newSolarSystem = arrayOf(
 )
 
 fun main() {
-    println(solarSystem[0])
-    println(solarSystem[1])
-    println(solarSystem[2])
-    println(solarSystem[3])
-    solarSystem[3] = "Little Earth"
-    println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
     println(newSolarSystem[8])
+    val solarSystem = mutableListOf(
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars",
+        "Jupiter",
+        "Saturn",
+        "Uranus",
+        "Neptune"
+    )
+    println(solarSystem.size)
+    println(solarSystem[2])
+    println(solarSystem.get(3))
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
+
+    for (planet in solarSystem) {
+        println(planet)
+    }
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+    solarSystem[3] = "Future Moon"
+    println(solarSystem[3])
+    println(solarSystem[9])
+    solarSystem.removeAt(9)
+    println(solarSystem.contains("Pluto"))
+    println("Future Moon" in solarSystem)
 }
